0b7e6775c2800073f580a61cb4d624eafa2ef7e0
Henok <hennok100@gmail.com>
2022-12-01 03:37:31 -0500

    intitial first

--- server.js
+++ server.js
@@ -12,14 +12,18 @@ app.use("/api/user", userRoute);
 app.use("/api/admin", adminRoute);
 app.use("/api/mechanic", mechanicRoute);
 
-if (process.env.NODE_ENV === "production") {
-  app.use("/", express.static("client/build"));
+// if (process.env.NODE_ENV === "production") {
+//   app.use("/", express.static("client/build"));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client/build/index.html"));
-  });
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client/build/index.html"));
+//   });
+// }
+// const port = process.env.PORT || 3001;
+
+// app.get("/", (req, res) => res.send("Hello World!"));
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
 }
-const port = process.env.PORT || 3001;
-
-app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(port, () => console.log(`Listening on port ${port}`));
